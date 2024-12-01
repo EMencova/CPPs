e1648fcde9574f9bdb5daba045f63c608f8ac659
emencova <eliskam@eliska.eliska>
2024-12-01 21:17:06 +0000

    added pre to main

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emencova <emencova@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eliskam <eliskam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 13:24:22 by emencova          #+#    #+#             */
-/*   Updated: 2024/12/01 13:13:53 by emencova         ###   ########.fr       */
+/*   Updated: 2024/12/01 21:15:15 by eliskam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,16 @@ int	main()
 	Bureaucrat *	bC = new Bureaucrat("bC", 65);
 	AForm *			shru = new ShrubberyCreationForm(target);
 	AForm *			rob = new RobotomyRequestForm(target);
+    AForm *			pre = new PresidentialPardonForm(target);
 
 	try
 	{
 		rob->beSigned(*bC);
 		shru->beSigned(*bB);
+        pre->beSigned(*bA);
 		rob->execute(*bA);
 		shru->execute(*bA);
+        pre->execute(*bA);
 	}
 	catch(const std::exception& e) {
 		std::cerr << e.what() << '\n';
@@ -42,11 +45,14 @@ int	main()
 	
 	std::cout << *shru << std::endl;
 	std::cout << *rob << std::endl;
+    std::cout << *pre << std::endl;
 
 	delete rob;
 	delete shru;
+    delete pre;
 	delete bC;
 	delete bB;
+    delete bA;
 }
 
 
