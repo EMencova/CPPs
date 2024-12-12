83b6142e7dc71cf144c5499f242fde2501bd8616
Eliska Mencova <emencova@c1r15s2.42porto.com>
2024-12-12 15:08:09 +0000

    subject

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -14,9 +14,11 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <exception>
 
 Base * generate(void)
 {
+    srand(time(NULL));
     int pick = std::rand() % 3;
 
     switch(pick)
@@ -43,38 +45,55 @@ Base * generate(void)
 
 void identify(Base* p)
 {
-    if (dynamic_cast<A*>(p))
+    if (dynamic_cast<A*>(p) != NULL)
         std::cout<<"A"<<std::endl;
-    else if (dynamic_cast<B*>(p))
+    else if (dynamic_cast<B*>(p) != NULL)
         std::cout<<"B"<<std::endl;
-    else if (dynamic_cast<C*>(p))
+    else if (dynamic_cast<C*>(p) != NULL)
         std::cout<<"C"<<std::endl;
 }
 
 void identify(Base& p)
 {
-    try 
-    {
-        (dynamic_cast<A&>(p))
-        std::cout<<"A"<<std::endl;
-    }
     try
     {
-        (dynamic_cast<B&>(p))
-        std::cout<<"B"<<std::endl;
+        A &a = dynamic_cast<A&>(p);
+        (void)a;
+        std::cout<<"A"<<std::endl;
     }
-    else if (dynamic_cast<C&>(p))
-        std::cout<<"C"<<std::endl;
+    catch(const std::exception &e)
+    {
+        try
+        {
+            B &b = dynamic_cast<B&>(p);
+            (void)b;
+            std::cout<<"B"<<std::endl;
+        }
+        catch(const std::exception &e)
+        {
+            try
+            { 
+                C &c = dynamic_cast<C&>(p);
+                (void)c;
+                std::cout<<"C"<<std::endl;
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr<< "Caught exception:" <<e.what()<< std::endl;
+            }
+        }
+    }
     
 }
 
-int	main() {
-	Base *	ptr = generate();
+int	main() 
+{
+    Base *	ptr = generate();
 
 	identify(ptr);
 	identify(*ptr);
 
 	delete ptr;
 
-	return 0;
+	return (0);
 }
