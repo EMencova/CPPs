d1a5326b2ff09b145c89b3d804814669f069ffb6
Eliska Mencova <emencova@c1r15s2.42porto.com>
2024-12-13 14:31:55 +0000

    cpp07

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -10,3 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+template <typename T>
+
+void swap(T &a, T &b)
+{
+    T temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+T min(T &a, T &b)
+{
+    if (a > b)
+     return (b);
+}
+
+T max (T &a, T &b)
+{
+    if (a < b)
+        return(a);
+}
